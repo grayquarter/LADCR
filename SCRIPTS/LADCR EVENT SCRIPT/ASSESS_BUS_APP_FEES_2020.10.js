b0ccfb6ad085aae3661fa9fe85950449d8790ea7
grayquarter <john@grayquarter.com>
2020-10-07 20:51:15 -0700

    Update ASSESS_BUS_APP_FEES_2020.10.js

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
@@ -7,6 +7,13 @@ if (!isTrue(AInfo["Is this a Renewal?"])) {
     updateFee("J300", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis Pre-Application Review Fee
 } 
 
+var amendFee = isTrue(AInfo["Legal Entity Name Change"]) || isTrue(AInfo["Business Premises Diagram"]) || isTrue(AInfo["Business Premises Relocation"]) || isTrue(AInfo["Ownership or Primary Changes"]) || isTrue(AInfo["Other"]);
+
+if (appMatch("Licenses/Cannabis/Application Amendment/Application") && amendFee) {
+	logDebug("adding amendment fee");
+    updateFee("J098B21", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis License Mod Fee
+} 
+
 function isTrue(o) {
     return String(o) == "CHECKED" || String(o) == "YES" || String(o) == "Yes" || String(o) == "Y";
 }
