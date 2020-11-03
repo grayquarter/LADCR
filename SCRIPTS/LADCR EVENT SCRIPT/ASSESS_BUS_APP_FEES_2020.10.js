7ccd90803398f7f0071c3de3b5c05f48e125773f
Gregg Hess <ghess@accela.com>
2020-11-03 14:59:23 -0800

    Update ASSESS_BUS_APP_FEES_2020.10.js
    Changed fee code from J300 to J102

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
@@ -1,11 +1,12 @@
 //ASSESS_BUS_APP_FEES_2020.10.js
-// Last Update 09/29/20
+// Last Update 11/03/20
+// Changed fee code from J300 to J102
 // This relies on AInfo[] being populated with custom fields.
 
 
 if (!isASITrue(AInfo["Is this a Renewal?"]) && (!appMatch("Licenses/Cannabis/Application Amendment/Application"))) {
     logDebug("Not a Renewal");
-    updateFee("J300", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis Pre-Application Review Fee
+    updateFee("J102", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis Pre-Application Review Fee
 } 
 
 
