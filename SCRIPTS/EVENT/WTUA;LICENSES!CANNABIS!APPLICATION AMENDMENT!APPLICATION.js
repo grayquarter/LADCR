ceafaad5f47ed9031c5bb8edfc23ddbf3c3fbf6a
grayquarter <john@grayquarter.com>
2021-01-04 12:30:28 -0800

    updated w/string

--- SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
@@ -1,23 +1,23 @@
-if (isTaskActive('Review') && matches(wfStatus,'Changes Denied')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Changes Denied')) {
 	closeTask('Close Out','Denied','Closed via script','');
 	}
 
-if (isTaskActive('Review') && matches(wfStatus,'Changes Accepted')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Changes Accepted')) {
 	closeTask('Close Out','Completed','Closed via script','');
 	}
 
-if (isTaskActive('Review') && matches(wfStatus,'Abandoned')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Abandoned')) {
 	closeTask('Close Out','Abandoned','Closed via script','');
 	}
 
-if (isTaskActive('Review') && matches(wfStatus,'Abandoned')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Abandoned')) {
 	closeTask('Close Out','Abandoned','Closed via script','');
 	}
 
-if (isTaskActive('Review') && matches(wfStatus,'Void')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Void')) {
 	closeTask('Close Out','Void','Closed via script','');
 	}
 
-if (isTaskActive('Review') && matches(wfStatus,'Withdrawn')) {
+if (isTaskActive('Review') && matches(String(wfStatus),'Withdrawn')) {
 	closeTask('Close Out','Withdrawn','Closed via script','');
     }
