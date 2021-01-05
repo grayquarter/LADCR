93e39a554731e4316f64ddbb0f3f7d0da16154f3
grayquarter <john@grayquarter.com>
2021-01-05 09:19:15 -0800

    changed script TT

--- SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
@@ -1,24 +1,24 @@
- if (isTaskActive('Review') && (wfStatus.equals('Changes Denied'))) {
+ if (wfTask == 'Review') {
+
+    if (wfStatus == 'Changes Denied') {
         closeTask('Close Out','Denied','Closed via script','');
         }
     
-    if (isTaskActive('Review') && (wfStatus.equals('Changes Accepted'))) {
+    if (wfStatus == 'Changes Accepted') {
         closeTask('Close Out','Completed','Closed via script','');
         logDebug("what is workflow status: " + wfStatus);
         }
     
-    if (isTaskActive('Review') && (wfStatus.equals('Abandoned'))) {
+    if (wfStatus == 'Abandoned') {
         closeTask('Close Out','Abandoned','Closed via script','');
         }
-    
-    if (isTaskActive('Review') && (wfStatus.equals('Abandoned'))) {
-        closeTask('Close Out','Abandoned','Closed via script','');
-        }
-    
-    if (isTaskActive('Review') && (wfStatus.equals('Void'))) {
+        
+    if (wfStatus == 'Void') {
         closeTask('Close Out','Void','Closed via script','');
         }
     
-    if (isTaskActive('Review') && (wfStatus.equals('Withdrawn'))) {
+    if (wfStatus == 'Withdrawn') {
         closeTask('Close Out','Withdrawn','Closed via script','');
         }
+
+}
