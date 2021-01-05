4c01307fe751621b94c59d6c812af4805b6bf1ab
grayquarter <john@grayquarter.com>
2021-01-04 16:22:59 -0800

    update TT

--- SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
+++ SCRIPTS/EVENT/WTUA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPLICATION.js
@@ -1,23 +1,23 @@
-if (isTaskActive('Review') && matches(String(wfStatus),'Changes Denied')) {
-	closeTask('Close Out','Denied','Closed via script','');
-	}
-
-if (isTaskActive('Review') && matches(String(wfStatus),'Changes Accepted')) {
-	closeTask('Close Out','Completed','Closed via script','');
-	}
-
-if (isTaskActive('Review') && matches(String(wfStatus),'Abandoned')) {
-	closeTask('Close Out','Abandoned','Closed via script','');
-	}
-
-if (isTaskActive('Review') && matches(String(wfStatus),'Abandoned')) {
-	closeTask('Close Out','Abandoned','Closed via script','');
-	}
-
-if (isTaskActive('Review') && matches(String(wfStatus),'Void')) {
-	closeTask('Close Out','Void','Closed via script','');
-	}
-
-if (isTaskActive('Review') && matches(String(wfStatus),'Withdrawn')) {
-	closeTask('Close Out','Withdrawn','Closed via script','');
-    }
+ if (isTaskActive('Review') && (wfStatus.equals('Changes Denied'))) {
+        closeTask('Close Out','Denied','Closed via script','');
+        }
+    
+    if (isTaskActive('Review') && (wfStatus.equals('Changes Accepted'))) {
+        closeTask('Close Out','Completed','Closed via script','');
+        }
+    
+    if (isTaskActive('Review') && (wfStatus.equals('Abandoned'))) {
+        closeTask('Close Out','Abandoned','Closed via script','');
+        }
+    
+    if (isTaskActive('Review') && (wfStatus.equals('Abandoned'))) {
+        closeTask('Close Out','Abandoned','Closed via script','');
+        }
+    
+    if (isTaskActive('Review') && (wfStatus.equals('Void'))) {
+        closeTask('Close Out','Void','Closed via script','');
+        }
+    
+    if (isTaskActive('Review') && (wfStatus.equals('Withdrawn'))) {
+        closeTask('Close Out','Withdrawn','Closed via script','');
+        }
