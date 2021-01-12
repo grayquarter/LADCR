e92f1d1b247bcf7b13479039fb86ccfc040e18fa
Gregg Hess <ghess@accela.com>
2021-01-12 14:53:18 -0800

    Create UPDATE_PRE-APP_TO_APP.js
    Initial create

---
+++ SCRIPTS/LADCR EVENT SCRIPT/UPDATE_PRE-APP_TO_APP.js
@@ -0,0 +1,9 @@
+//Drop preapp extension
+
+//Create child activity record
+
+//Fees – applied to parent (defined)
+
+//Notify applicant – Your application is eligible for…
+
+//They are now allowed to add documents to the record.
