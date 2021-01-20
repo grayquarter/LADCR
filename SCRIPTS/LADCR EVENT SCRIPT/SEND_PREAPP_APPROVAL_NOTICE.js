35cbefd3a605cd77168533da66ce0bf0dbebcdef
Gregg Hess <ghess@accela.com>
2021-01-20 14:55:33 -0800

    Create SEND_PREAPP_APPROVAL_NOTICE.js
    initial creation

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_PREAPP_APPROVAL_NOTICE.js
@@ -0,0 +1,7 @@
+if (wfTask == "Supervisor Pre-App Document Review" && wfStatus == "Eligible for Processing") {
+	var vEParams = aa.util.newHashtable();
+	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+	//addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	emailContacts_BCC("All", "DCA PREAPP LICENSE APPROVED NOTIFICATION", vEParams, "", "");
+}
