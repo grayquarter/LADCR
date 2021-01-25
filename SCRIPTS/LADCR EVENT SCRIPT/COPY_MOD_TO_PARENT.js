dd6691c762d4c0e5314ed46b9b7b5edb20de1642
grayquarter <john@grayquarter.com>
2021-01-25 12:26:22 -0800

    UPDATED TO ADD IN ASI TT

--- SCRIPTS/LADCR EVENT SCRIPT/COPY_MOD_TO_PARENT.js
+++ SCRIPTS/LADCR EVENT SCRIPT/COPY_MOD_TO_PARENT.js
@@ -1,22 +1,45 @@
 if (wfTask.equals("Review") && wfStatus.equals("Changes Accepted")) {
-	if (parentCapId) {
-	
-		//copy ASI infomation
-		var AppSpecInfo = new Array();
-		loadAppSpecific(AppSpecInfo, capId);
-
-		for (asi in AppSpecInfo) {
-			if (AppSpecInfo[asi] != null && AppSpecInfo[asi] != 'undefined') {
-				logDebug("Copying ASI Field: " + asi + " Value: " + AppSpecInfo[asi]);
-				editAppSpecific(asi, AppSpecInfo[asi], parentCapId);
-			}
-		}
-
-		// copy over contacts 
-        copyContacts3_0(capId, parentCapId);
         
-        // copy conditions over from mod request
-        copyConditions(capId, parentCapId);
-	}
-}
+        if (parentCapId) {
+                
+            if (isASITrue(AInfo["Remove Cannabis Activity"]) || isASITrue(AInfo["New Cannabis Activity"])) {
+            //copy ASI infomation, but I want to replace as well. 
+            // there is a standard function to do that... copyASIFields(sourceCapId, targetCapId); in you case:
+            
+            copyASIFields(capId, parentCapId)
+            
+           }
+    
+        if (isASITrue(AInfo["Ownership or Primary Changes"]) || isASITrue(AInfo["Other Contact Changes"])) {
+                // copy over contacts from child
+                  //  remove all contacts from parent
+                capContactResult = aa.people.getCapContactByCapID(parentCapId);
+                if (capContactResult.getSuccess()) {
+                    var contacts = capContactResult.getOutput();
+                    for (var i in contacts) {
+    
+                        //REMOVING CONTACTS
+                        if (contacts[i].getPeople()) {
+                            var capContactNumber = aa.util.parseInt(contacts[i].getCapContactModel().getPeople().getContactSeqNumber());
+                            aa.people.removeCapContact(parentCapId, capContactNumber);
+                            logDebug(contacts[i].getPeople().getContactType() + " - Contact Seq Number " + capContactNumber + " removed from parent " + parentCapId);
+                        }
+                    }
+                }
+            
+            copyContacts3_0(capId, parentCapId);
+        }
+            // copy conditions over from mod request
+            copyConditions(capId, parentCapId);
+    
+            //copy DBA and legal name 
+    
+        if (isASITrue(AInfo["Fictitious Business Name"]) || isASITrue(AInfo["Legal Entity Name Change"])) { 
+    
+            editAppSpecific("Legal Business Name",AInfo["Legal Entity Name Change",parentCapId]);
+            editAppSpecific("DBA Name",AInfo["Fictitious Business Name",parentCapId]);
+    
+          }
+        }
+    }
 // End script to copy mod request info back to parent
