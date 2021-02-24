b49855e8978a6c2f062b71513b12979d773ef354
Gregg Hess <ghess@accela.com>
2021-02-24 14:38:10 -0800

    Update ASSESS_BUS_APP_FEES_2020.10.js
    Changed fee codes to match new fee schedule version 186708_V1

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
@@ -1,14 +1,21 @@
 //ASSESS_BUS_APP_FEES_2020.10.js
 // Last Update 02/24/21
-// Changed fee code for Mod to match new fee schedule
+// Changed fee codes to match new fee schedule version 186708_V1 
 // This relies on AInfo[] being populated with custom fields.
 
 
 if (!isASITrue(AInfo["Is this a Renewal?"]) && (!appMatch("Licenses/Cannabis/Application Amendment/Application"))) {
     logDebug("Not a Renewal");
-    updateFee("J102", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis Pre-Application Review Fee
-} 
 
+	if (isASITrue("Retailer Commercial Cannabis Activity license in an area of Undue Concentration?")) {
+	{
+		logDebug("adding PCN fee");
+		updateFee("J097", "CAN_BUS_APP", "FINAL", 1, "Y", null,null,capId); 
+	} else {
+		logDebug("adding Pre-App fee");
+		updateFee("J102", "CAN_BUS_APP", "FINAL", 1, "Y", null,null,capId);
+	}
+} 
 
 var amendFee = isASITrue(AInfo["Legal Entity Name Change"]) || isASITrue(AInfo["Business Premises Diagram"]) || isASITrue(AInfo["Business Premises Relocation"]) || isASITrue(AInfo["Ownership or Primary Changes"]) || isASITrue(AInfo["Other"]);
 
