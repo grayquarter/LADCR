04bcb26f5b9da44c689d77f87029176e86133289
Gregg Hess <ghess@accela.com>
2021-02-25 08:57:22 -0800

    Update ASSESS_BUS_APP_FEES_2020.10.js
    fix

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
@@ -8,7 +8,6 @@ if (!isASITrue(AInfo["Is this a Renewal?"]) && (!appMatch("Licenses/Cannabis/App
     logDebug("Not a Renewal");
 
 	if (isASITrue("Retailer Commercial Cannabis Activity license in an area of Undue Concentration?")) {
-	{
 		logDebug("adding PCN fee");
 		updateFee("J097", "CAN_BUS_APP", "FINAL", 1, "Y", null,null,capId); 
 	} else {
