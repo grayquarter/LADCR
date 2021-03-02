3af73fd7d06a6e7138e1e52f674e72836a64a62f
Gregg Hess <ghess@accela.com>
2021-03-01 16:30:15 -0800

    Update SEND_PREAPP_APPROVAL_NOTICE.js
    updated

--- SCRIPTS/LADCR EVENT SCRIPT/SEND_PREAPP_APPROVAL_NOTICE.js
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_PREAPP_APPROVAL_NOTICE.js
@@ -1,7 +1,11 @@
 //if (wfTask == "Supervisor Pre-App Document Review" && wfStatus == "Eligible for Processing") {
 	var vEParams = aa.util.newHashtable();
 	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
-	//addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
 	addParameter(vEParams, "$$ApplicationID$$", capIDString);
-	emailContacts_BCC("All", "DCR ELIGIBLE FOR PROCESSING NOTICE", vEParams, "", "");
+	
+	var vRParams = aa.util.newHashtable();
+	addParameter(vRParams, "p1Value", capIDString);
+	
+	emailContacts_BCC("All", "DCR ELIGIBLE FOR PROCESSING NOTICE", vEParams, "Invoice – Application as Doc", vRParams);
 //}
