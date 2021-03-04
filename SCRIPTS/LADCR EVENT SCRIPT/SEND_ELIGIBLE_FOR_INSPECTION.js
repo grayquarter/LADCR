7d55870a1e65edfa034fdab66d7dd1bd1662aaf9
Gregg Hess <ghess@accela.com>
2021-03-03 16:15:49 -0800

    Create SEND_ELIGIBLE_FOR_INSPECTION.js
    initialize

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_ELIGIBLE_FOR_INSPECTION.js
@@ -0,0 +1,10 @@
+//SEND_ELIGIBLE_FOR_INSPECTION
+	var vEParams = aa.util.newHashtable();
+	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	
+	var vRParams = aa.util.newHashtable();
+	addParameter(vRParams, "p1Value", capIDString);
+	
+	emailContacts_BCC("All", "DCR Eligible for Inspection Notice", vEParams, "Notice of Local Complianc as Doc", vRParams);
