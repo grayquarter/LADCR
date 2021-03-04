dae1dfbfd34265680a4722f116d1b1751cf98280
Gregg Hess <ghess@accela.com>
2021-03-03 16:22:15 -0800

    Create SEND_APPEAL_AND_NOTICE.js
    initialize

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_APPEAL_AND_NOTICE.js
@@ -0,0 +1,10 @@
+//SEND_APPEAL_AND_NOTICE
+	var vEParams = aa.util.newHashtable();
+	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	
+	var vRParams = aa.util.newHashtable();
+	addParameter(vRParams, "p1Value", capIDString);
+	
+	emailContacts_BCC("All", "DCR Appeal Notice and Invoice", vEParams, "Invoice - Appeal as Doc", vRParams);
