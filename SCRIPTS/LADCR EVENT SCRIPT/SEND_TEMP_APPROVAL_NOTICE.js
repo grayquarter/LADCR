4734862abb07abf4d5cff8b49dec3ea0a9310aac
Gregg Hess <ghess@accela.com>
2021-03-03 16:19:11 -0800

    Create SEND_TEMP_APPROVAL_NOTICE.js
    initialize

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_TEMP_APPROVAL_NOTICE.js
@@ -0,0 +1,10 @@
+//SEND_TEMP_APPROVAL_NOTICE
+	var vEParams = aa.util.newHashtable();
+	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	
+	var vRParams = aa.util.newHashtable();
+	addParameter(vRParams, "p1Value", capIDString);
+	
+	emailContacts_BCC("All", "DCR Temporary Approval Notice", vEParams, "Temporary Approval Licens as Doc", vRParams);
