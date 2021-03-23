a724714090c09a583877d4f503da8c527f3de0c6
Gregg Hess <ghess@accela.com>
2021-03-22 18:07:20 -0700

    Create SEND_INTERESTED_PARTIES_PCN.js
    initial load

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_INTERESTED_PARTIES_PCN.js
@@ -0,0 +1,15 @@
+//SEND_INTERESTED_PARTIES_PCN
+	var vEParams = aa.util.newHashtable();
+	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	
+  addParameter(vEParams, "$$CommunityPlanArea$$, AInfo["Community Plan Area"]);
+  addParameter(vEParams, "$$CouncilFileNo$$, AInfo["Council File Number"]);
+  addParameter(vEParams, "$$CouncilDistrict$$, AInfo["Council District"]);
+
+	var vRParams = aa.util.newHashtable();
+	addParameter(vRParams, "p1Value", capIDString);
+	
+  //us4-8362312074-aff97321c2@inbound.mailchimp.com
+	emailAsync_BCC("ghess@accela.com", "DCR Interested Parties Notification PCN", vEParams, "", "");
