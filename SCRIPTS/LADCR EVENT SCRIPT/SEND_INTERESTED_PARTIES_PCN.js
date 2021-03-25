ac1e75d6d5b4b254eafe4e896d8d4627b744d4b0
Gregg Hess <ghess@accela.com>
2021-03-25 12:15:21 -0700

    Update SEND_INTERESTED_PARTIES_PCN.js
    added address parameters

--- SCRIPTS/LADCR EVENT SCRIPT/SEND_INTERESTED_PARTIES_PCN.js
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_INTERESTED_PARTIES_PCN.js
@@ -1,15 +1,63 @@
 //SEND_INTERESTED_PARTIES_PCN
+
+	// Email Notification parameters
 	var vEParams = aa.util.newHashtable();
 	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
 	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
 	addParameter(vEParams, "$$ApplicationID$$", capIDString);
 	
-  addParameter(vEParams, "$$CommunityPlanArea$$", AInfo["Community Plan Area"]);
-  addParameter(vEParams, "$$CouncilFileNo$$", AInfo["Council File Number"]);
-  addParameter(vEParams, "$$CouncilDistrict$$", AInfo["Council District"]);
+	addParameter(vEParams, "$$CommunityPlanArea$$", AInfo["Community Plan Area"]);
+	addParameter(vEParams, "$$CouncilFileNo$$", AInfo["Council File Number"]);
+	addParameter(vEParams, "$$CouncilDistrict$$", AInfo["Council District"]);
 
+	vAddresses = getAddress(capId);
+	if (vAddresses) {
+		x = 0;
+		for (x in vAddresses) {
+			vAddress = vAddresses[x];
+
+			if (vAddress) {
+				var strAddress1 = vAddress.getHouseNumberStart();
+				var addPart = vAddress.getStreetDirection();
+				if (addPart && addPart != "") 
+					strAddress1 += " " + addPart;
+				var addPart = vAddress.getStreetName();
+				if (addPart && addPart != "") 
+					strAddress1 += " " + addPart;	
+				var addPart = vAddress.getStreetSuffix();
+				if (addPart && addPart != "") 
+					strAddress1 += " " + addPart;	
+
+				var strAddress2 = vAddress.getUnitType();
+				var addPart = vAddress.getUnitStart();
+				if (addPart && addPart != "") 
+					strAddress2 += " " + addPart;
+					
+				var strCityStateZip = vAddress.getCity();
+				var addPart = vAddress.getState();
+				if (addPart && addPart != "") 
+					strCityStateZip += ", " + addPart;	
+				var addPart = vAddress.getZip();
+				if (addPart && addPart != "") 
+					strCityStateZip += " " + addPart;	
+			}
+
+			logDebug( "strAddress1= " + strAddress1);
+			logDebug( "strAddress2= " + strAddress2);
+			logDebug( "strCityStateZip= " + strCityStateZip);
+
+			addParameter(vEParams, "$$address1$$", strAddress1);
+			addParameter(vEParams, "$$address2$$", strAddress1);
+			addParameter(vEParams, "$$cityStateZip$$", strAddress1);
+			
+			// Exit loop - assumes only one Business address type
+			break;
+		}
+	}
+	
+	//Report parameter - not used
 	var vRParams = aa.util.newHashtable();
 	addParameter(vRParams, "p1Value", capIDString);
-	
-  //us4-8362312074-aff97321c2@inbound.mailchimp.com
+
+  	//us4-8362312074-aff97321c2@inbound.mailchimp.com
 	emailAsync_BCC("ghess@accela.com", "DCR Interested Parties Notification PCN", vEParams, "", "");
