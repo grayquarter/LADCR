206eb239f791380c8368214cdd896d03197b8346
Sal Guerrero <sal@grayquarter.com>
2021-04-12 16:21:25 -0700

    .

--- INCLUDES_CUSTOM/emailContacts_BCC.js
+++ INCLUDES_CUSTOM/emailContacts_BCC.js
@@ -96,7 +96,7 @@ logDebug("Validated contact types to send to: " + sendEmailToContactTypes);
 	}
 	else {
 		//call sendEmailASync script
-		debugSlack("Testing update here")
+		aa.print("REACHED HERE.")
 		logDebug("Attempting to run Async: " + vAsyncScript);
 		aa.runAsyncScript(vAsyncScript, envParameters);
 		/*
