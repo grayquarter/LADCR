8d553a42b99c48b4848886a76a6668aaf53acb74
Gregg Hess <ghess@accela.com>
2021-04-29 15:12:27 -0700

    Update SEND_ELIGIBLE_FOR_INSPECTION.js
    cleanup

--- SCRIPTS/LADCR EVENT SCRIPT/SEND_ELIGIBLE_FOR_INSPECTION.js
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_ELIGIBLE_FOR_INSPECTION.js
@@ -8,6 +8,5 @@
 	addParameter(vRParams, "p1Value", capIDString);
 	
 	//emailContacts_BCC("All", "DCR Eligible for Inspection Notice", vEParams, "Notice of Local Complianc as Doc", vRParams);
-	//emailContactsAttachDoc_BCC(sendEmailToContactTypes, emailTemplate, vEParams, reportTemplate, vRParams, vAddHocTask, vChangeReportName, vContactCapId, attachDocs) 
-	//emailContactsAttachDoc_BCC("All", "DCR Eligible for Inspection Notice", vEParams, "Notice of Local Complianc as Doc", vRParams, "N", null, capId, true); //04/23/2021
+	//use new function to attach report to email
 	emailContactsAttachDoc_BCC("All", "DCR Eligible for Inspection Notice", vEParams, "Notice of Local Complianc as Doc", vRParams, "N", "", "", true);
