2d2f4e12e73f0e48517f35df4c51bcaab3d3ff9c
grayquarter <john@grayquarter.com>
2021-05-13 13:10:18 -0700

    Update GRANT_RECIPIENT_STATUS.js

--- SCRIPTS/LADCR EVENT SCRIPT/GRANT_RECIPIENT_STATUS.js
+++ SCRIPTS/LADCR EVENT SCRIPT/GRANT_RECIPIENT_STATUS.js
@@ -1,29 +1,30 @@
-var recieved = false;
-var refContactNbr = ContactObj.getRefContactNumber();
-var ContactAttr = new Array();
-var pa = ContactObj.getPeople().getAttributes().toArray();
-var retValue = "Finished.";
-for (xx1 in pa){
-    ContactAttr[pa[xx1].attributeName] = pa[xx1].attributeValue;
-    logDebug("ContactAttr[\"" + pa[xx1].attributeName + "\"] = " + pa[xx1].attributeValue);
-}
-if(!(matches(ContactAttr["GRANT RECIPIENT STATUS"],null,undefined,""))) {
-    editAppSpecific("Grant Recipient", "Yes");
-    editAppSpecific("Grant Recipient Reference Contact ID", refContactNbr);
-    var asi1 = getAppSpecific("Grant Recipient", capId);
-    var asi2= getAppSpecific("Grant Recipient Reference Contact ID", capId);
-    recieved = true;
-    logDebug("Set ASI values Grant Recipient to 'Yes' and Grant Recipient Reference Contact ID to " + refContactNbr + ", Grant status recieved: " + recieved);
-} else {
-    editAppSpecific("Grant Recipient", "No");
-    editAppSpecific("Grant Recipient Reference Contact ID", "");
-    recieved = false;
-    logDebug("Set ASI values to 'No' and '' grant status recieved: " + recieved);
-}
+if (matches(contactType, "Social Equity Owner")) {
+    var recieved = false;
+    var refContactNbr = ContactObj.getRefContactNumber();
+    var ContactAttr = new Array();
+    var pa = ContactObj.getPeople().getAttributes().toArray();
+    var retValue = "Finished.";
+    for (xx1 in pa) {
+        ContactAttr[pa[xx1].attributeName] = pa[xx1].attributeValue;
+        logDebug("ContactAttr[\"" + pa[xx1].attributeName + "\"] = " + pa[xx1].attributeValue);
+    }
+    if (!(matches(ContactAttr["GRANT RECIPIENT STATUS"], null, undefined, ""))) {
+        editAppSpecific("Grant Recipient", "Yes");
+        editAppSpecific("Grant Recipient Reference Contact ID", refContactNbr);
+        var asi1 = getAppSpecific("Grant Recipient", capId);
+        var asi2 = getAppSpecific("Grant Recipient Reference Contact ID", capId);
+        recieved = true;
+        logDebug("Set ASI values Grant Recipient to 'Yes' and Grant Recipient Reference Contact ID to " + refContactNbr + ", Grant status recieved: " + recieved);
+    } else {
+        editAppSpecific("Grant Recipient", "No");
+        editAppSpecific("Grant Recipient Reference Contact ID", "");
+        recieved = false;
+        logDebug("Set ASI values to 'No' and '' grant status recieved: " + recieved);
+    }
 
-if (refContactNbr && refContactNbr != "") {
-    refConResult = aa.people.getPeople(refContactNbr);
-    if (refConResult.getSuccess()) {
+    if (refContactNbr && refContactNbr != "") {
+        refConResult = aa.people.getPeople(refContactNbr);
+        if (refConResult.getSuccess()) {
             refPeopleModel = refConResult.getOutput();
             refAttrResult = aa.people.getPeopleAttributeByPeople(refContactNbr, refPeopleModel.getContactType());
             if (refAttrResult.getSuccess()) {
@@ -31,7 +32,7 @@ if (refContactNbr && refContactNbr != "") {
                 for (var i in refAttr) {
                     item = refAttr[i];
                     if ("GRANT RECIPIENT".equals(item.getAttributeName())) {
-                        if(recieved) {
+                        if (recieved) {
                             item.setAttributeValue("Yes");
                             logDebug("Set contact attribute to Yes");
                         } else {
@@ -40,21 +41,21 @@ if (refContactNbr && refContactNbr != "") {
                         }
                         paEditResult = aa.people.editPeopleAttribute(item.getPeopleAttributeModel());
                         if (!paEditResult.getSuccess()) {
-                            retValue =  "failure - attribute edit failed " + paEditResult.getErrorMessage();
+                            retValue = "failure - attribute edit failed " + paEditResult.getErrorMessage();
                         } else {
                             logDebug("Changed " + refContactNbr);
                         }
                     }
                 }
-            } else { 
-                retValue =  "failure - failed to get attributes " + refAttrResult.getErrorMessage();
+            } else {
+                retValue = "failure - failed to get attributes " + refAttrResult.getErrorMessage();
             }
-    } else { 
-        retValue =  "failure - failed to retrieve contact " + refConResult.getErrorMessage();
+        } else {
+            retValue = "failure - failed to retrieve contact " + refConResult.getErrorMessage();
+        }
+    } else {
+        retValue = "failure - reference contact number not passed";
     }
-} else { 
-    retValue =  "failure - reference contact number not passed";
-}
 
-logDebug(retValue);
-    
+    logDebug(retValue);
+}
