9d2444fe44498258aa125278ff1e4ff0e4afc87f
grayquarter <john@grayquarter.com>
2021-05-20 12:35:25 -0700

    imported neighbors

--- SCRIPTS/EXPRESSION/ADDRESS_ZIMAS.js
+++ SCRIPTS/EXPRESSION/ADDRESS_ZIMAS.js
@@ -86,9 +86,92 @@ function getZimasDataFromAddress(street, nbr, dir) {
 		//  aa.print(vOut);
 		// not sure if we need this JSON.parse, getOutput might do this already
 		var vOutParsed = JSON.parse(vOut);
-
-		return vOutParsed;
+		logDebug("returned " + vOutParsed.length + " results");
+		response.addressResults = String(vOutParsed.length);
+		//return vOutParsed;
 	}
+
+	// only get ZIMAS data if we have a known good address
+	if (vOutParsed.length == 1) {
+
+		theUrl = encodeURI(dataURL + "PIN=" + String(vOutParsed[0].PIN));
+		response.dataurl = String(theUrl);
+
+		var vOutObj = aa.httpClient.get(theUrl, header);
+
+		if (vOutObj.getSuccess()) {
+			var vOut = vOutObj.getOutput();
+			var vOutParsed = JSON.parse(vOut);
+			if (vOutParsed.length > 0) {
+
+				logDebug("we have data");
+				response.data = {};
+				//var addrData = vOutParsed[0].Value.ZIMASDataTabs[0];
+				var jurisData = vOutParsed[0].Value.ZIMASDataTabs[1];
+				var zoningData = vOutParsed[0].Value.ZIMASDataTabs[2];
+				var econoData = vOutParsed[0].Value.ZIMASDataTabs[8];
+				var safetyData = vOutParsed[0].Value.ZIMASDataTabs[10];
+				if (jurisData) {
+					for (var i in jurisData.Value) {
+						if ("Community Plan Area".equals(jurisData.Value[i].Description)) {
+							response.data["Community Plan Area"] = String(jurisData.Value[i].Value);
+						}
+						if ("Council District".equals(jurisData.Value[i].Description)) {
+							response.data["Council District"] = String(jurisData.Value[i].Value);
+							var councilDist = String(jurisData.Value[i].Value);
+							if (councilDist.indexOf("-") > 0) {
+								councilDist = councilDist.slice(0,councilDist.indexOf("-")-1)
+								//logDebug("councilDist = '" + councilDist + "'");
+								response.data["Council District"] = councilDist;
+							}
+						}
+						if ("Neighborhood Council".equals(jurisData.Value[i].Description)) {
+							response.data["Neighborhood Council"] = String(jurisData.Value[i].Value);
+						}
+						if ("Area Planning Commission".equals(jurisData.Value[i].Description)) {
+							response.data["Area Planning Commission"] = String(jurisData.Value[i].Value);
+						}
+					}
+				}
+				var zone = [];
+				if (zoningData) {
+					for (var i in zoningData.Value) {
+
+						if ("CPIO: Community Plan Imp. Overlay".equals(zoningData.Value[i].Description)) {
+							response.data["Overlay Zone"] = String(zoningData.Value[i].Value);
+						}
+						if ("Specific Plan Area".equals(zoningData.Value[i].Description)) {
+							response.data["Specific Plan Area"] = String(zoningData.Value[i].Value);
+						}
+						if ("Zoning".equals(zoningData.Value[i].Description)) {
+							zone.push(String(zoningData.Value[i].Value));
+						}
+					}
+				}
+				response.data["Zoning"] = String(zone.join(","));
+				if (econoData) {
+					for (var i in econoData.Value) {
+
+						if ("Business Improvement District".equals(econoData.Value[i].Description)) {
+							response.data["Business Improvement District"] = String(econoData.Value[i].Value);
+						}
+					}
+				}
+				if (safetyData) {
+					for (var i in safetyData.Value) {
+						if (safetyData.Value[i].Description.indexOf("Division / Station") >= 0) {
+							response.data["LAPD Area Station"] = String(safetyData.Value[i].Value);
+						}
+						if (safetyData.Value[i].Description.indexOf("District / Fire Station") >= 0) {
+							response.data["LAFD Fire Station"] = String(safetyData.Value[i].Value);
+						}
+
+					}
+				}
+			}
+		}
+	}
+	return response;
 }
 
 function lookup(stdChoice,stdValue) {
