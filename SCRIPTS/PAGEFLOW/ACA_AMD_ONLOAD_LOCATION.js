1af785679b8c518fbfcf510ab2dec1b045a6de6f
Tosh <66090718+toshtini@users.noreply.github.com>
2021-06-21 15:57:52 -0700

    pp

--- SCRIPTS/PAGEFLOW/ACA_AMD_ONLOAD_LOCATION.js
+++ SCRIPTS/PAGEFLOW/ACA_AMD_ONLOAD_LOCATION.js
@@ -148,6 +148,7 @@ logDebug("balanceDue = " + balanceDue);
 // page flow custom code begin
 
 try {
+ showDebug = true; 
 
 	parentCapIdString = "" + cap.getParentCapID();
 	if (parentCapIdString) {
@@ -195,7 +196,34 @@ try {
 	if (!showPage) {
 		aa.env.setValue("ReturnData", "{'PageFlow': {'HidePage' : 'Y'}}");
 	}
-	
+
+
+
+  
+    editAppSpecific("Adult-Use Cultivation Medium Indoor1", getAppSpecific("Adult-Use Cultivation Medium Indoor", parentCapId));
+    editAppSpecific("Adult-Use Cultivation Small Indoor1", getAppSpecific("Adult-Use Cultivation Small Indoor", parentCapId));
+    editAppSpecific("Adult-Use Cultivation Specialty Cottage Indoor1", getAppSpecific("Adult-Use Cultivation Specialty Cottage Indoor", parentCapId));
+    editAppSpecific("Adult-Use Cultivation Specialty Indoor1", getAppSpecific("Adult-Use Cultivation Specialty Indoor", parentCapId));
+    editAppSpecific("Adult-Use Distributor1", getAppSpecific("Adult-Use Distributor", parentCapId));
+    editAppSpecific("Adult-Use Manufacturer Level 11", getAppSpecific("Adult-Use Manufacturer Level 1", parentCapId));
+logDebug("manu");
+    editAppSpecific("Adult-Use Manufacturer Level 21", getAppSpecific("Adult-Use Manufacturer Level 2", parentCapId));
+    editAppSpecific("Adult-Use Retail1", getAppSpecific("Adult-Use Retail", parentCapId));
+    editAppSpecific("Adult-Use Microbusiness1", getAppSpecific("Adult-Use Microbusiness", parentCapId));
+    editAppSpecific("Adult-Use Delivery Only1", getAppSpecific("Adult-Use Delivery Only", parentCapId));
+    editAppSpecific("Medical Cultivation Medium Indoor1", getAppSpecific("Medical Cultivation Medium Indoor", parentCapId));
+    editAppSpecific("Medical Cultivation Small Indoor1", getAppSpecific("Medical Cultivation Small Indoor", parentCapId));
+    editAppSpecific("Medical Distributor1", getAppSpecific("Medical Distributor", parentCapId));
+    editAppSpecific("Medical Cultivation Specialty Indoor1", getAppSpecific("Medical Cultivation Specialty Indoor", parentCapId));
+    editAppSpecific("Medical Manufacturer Level 11", getAppSpecific("Medical Manufacturer Level 1", parentCapId));
+    editAppSpecific("Medical Manufacturer Level 21", getAppSpecific("Medical Manufacturer Level 2", parentCapId));
+    editAppSpecific("Medical Retail1", getAppSpecific("Medical Retail", parentCapId));
+    editAppSpecific("Medical Microbusiness1", getAppSpecific("Medical Microbusiness", parentCapId));
+    editAppSpecific("Medical Delivery Only1", getAppSpecific("Medical Delivery Only", parentCapId));
+    editAppSpecific("Distributor Transport Only1", getAppSpecific("Distributor Transport Only", parentCapId));
+    editAppSpecific("Testing1", getAppSpecific("Testing", parentCapId));
+logDebug("this is running");
+    editAppSpecific("Nursery1", getAppSpecific("Nursery", parentCapId));
 } catch (err) {
 
 	logDebug(err);
